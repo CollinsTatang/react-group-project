8b8a46e70892c140e2e736a3abb3c6bc286b4fe6
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-08-31 18:04:47 +0200

    Add components

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
-const Rockets = () => (
-  <section>
-    <p>rockets page is here!</p>
-  </section>
-);
+function Rockets() {
+  const { reservationReducer } = useSelector((state) => state);
+  const { reservations } = reservationReducer;
+  const { rockets } = reservations;
+  const [rocketsDisplay, setRocketsDisplay] = useState(null);
+
+  useEffect(() => {
+    if (rockets !== rocketsDisplay) {
+      setRocketsDisplay(rockets);
+    }
+  });
+
+  if (rocketsDisplay) {
+    return (
+      <div className="rocketsContainer">
+        {rocketsDisplay.map((element) => (
+          <div key={element.id}>
+            <img src={element.flickr_images[0]} alt="rocket" />
+            <div>
+              <h1>{element.rocket_name}</h1>
+              <p>{element.description}</p>
+              <button type="button">Reserve</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  return <h3>empty</h3>;
+}
 
 export default Rockets;
