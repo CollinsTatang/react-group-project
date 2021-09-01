3e2b2b256de93d96408bf4e14ba6fe69f8f3cddd
Collins Tatang <makungongcollins@gmail.com>
2021-09-01 14:41:42 +0100

    Add rockets handler and action

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { addReservation } from '../redux/reservations/reservations';
 
 function Missions() {
-  const dispatch = useDispatch();
-  const submit = () => {
-    const hello = 'hello';
-    dispatch(addReservation(hello));
-  };
-
   return (
-    <section>
-      <p>missions page is here!</p>
-      <button type="button" onClick={submit}>add reservation</button>
-    </section>
+    <p>missions page is here!</p>
   );
 }
 
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,26 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bookRocket, cancelBooking, fetchRockets } from '../redux/rockets/rockets';
 
-function Rockets() {
-  const { reservationReducer } = useSelector((state) => state);
-  const { reservations } = reservationReducer;
-  const { rockets } = reservations;
-  const [rocketsDisplay, setRocketsDisplay] = useState(null);
-  console.log(rockets);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets);
+
   useEffect(() => {
-    if (rockets !== rocketsDisplay) {
-      setRocketsDisplay(rockets);
+    if (!rockets.length) {
+      dispatch(fetchRockets);
     }
-  });
+  }, []);
 
-  if (rocketsDisplay) {
-    return (
-      <div className="rocketsContainer">
-        <p>hello</p>
+  const handleBooking = (id) => dispatch(bookRocket(id));
+  const handleCancellation = (id) => dispatch(cancelBooking(id));
+
+  return (
+    rockets.map(({
+      id, name, description, images, reserved,
+    }) => (
+      <div key={id} className="">
+        <div>
+          <img src={images[0]} alt="roc-img" />
+        </div>
+        <div>
+          <h3>{name}</h3>
+          <p>
+            {description}
+          </p>
+          {reserved && (
+            <button type="button" onClick={() => handleCancellation(id)}>
+              Cancel reservation
+            </button>
+          )}
+          {!reserved && (
+            <button type="button" onClick={() => handleBooking(id)}>
+              Reserve rocket
+            </button>
+          )}
+        </div>
       </div>
-    );
-  }
-  return <h3>empty</h3>;
-}
+    ))
+  );
+};
 
 export default Rockets;
